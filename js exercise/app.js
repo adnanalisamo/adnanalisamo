4ed0004cc1798b9bb5b8cb1db94c7771f95417d3
Adnan ali <153611386+adnanalisamo@users.noreply.github.com>
2024-01-29 00:36:33 +0500

    Add files via upload

---
+++ js exercise/app.js
@@ -0,0 +1,91 @@
+        // Ans 1
+ alert("Wellcome to our Website");
+
+
+
+        // Ans 2
+console.error(" Please Enter a valid password");
+
+
+        // Ans 3
+alert("Wellcome to . <br> our Website");
+
+
+        // Ans 4
+alert("Please enter a valid password <br> Please try again <br> this age does not match");
+
+
+
+        // Ans 7
+var username;
+
+
+
+        // Ans 8
+var myName= "Adnan ali";
+
+
+
+
+//        Ans 9
+// A
+var msg= "be posative";
+document.write(msg);
+
+// B
+var x= "Hello world";
+document.write(x);
+
+// C
+alert("Be posative")
+
+
+
+//      Ans 10    
+var studentName= "Adnan ali"
+var age = 20;
+var grade= "A"
+
+
+
+
+//        Ans 11
+alert("salamsindh40000@gmail.com");
+
+
+
+
+//      Ans 12
+var p="Hello guys";
+document.write(p);
+
+
+
+
+//        Ans 13
+var  x="Hello";
+alert(x);
+document.write(x);
+
+
+ 
+//        Ans 14
+var z= "Show your age";
+document.write(z);
+alert(z);
+
+
+
+//        Ans 16
+var A= "Year of birth 2006";
+document.write(A);
+
+
+//     Ans 18
+var s= 15;
+var b= 16;
+var c= 17;
+document.write(s+b-c);
+ 
+
+
