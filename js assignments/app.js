a670b7f42b2e8203b35107654013772792c46fea
Adnan ali <153611386+adnanalisamo@users.noreply.github.com>
2024-02-05 23:17:46 +0500

    Add files via upload
    update file

---
+++ js assignments/app.js
@@ -0,0 +1,108 @@
+        // Assignment 2
+
+// Ans 1
+
+// var a = 10;
+// var b = 1;
+// var b = 1;
+// console.log (++a);
+
+// var a = 11;
+// var b = 1;
+// var b = 1;
+// console.log (++a);
+
+
+//  var a = 12;
+// var b = 1;
+// var b = 1;
+// console.log (--a);
+
+//  var a = 11;
+// var b = 1;
+// var b = 1;
+// console.log (--a);
+
+
+        // Ans 2
+
+// var a=20;
+// var b=15;
+
+// console.log(--a);
+
+// var a=20;
+// var b=15;
+
+// console.log(--b);
+
+
+// var a=20;
+// var b=15;
+
+// console.log(++b);
+
+// var a=20;
+// var b=15;
+
+// console.log(+b--);
+
+// var a=20;
+// var b=15;
+
+// console.log(a--);
+
+// A is 20 
+// b is 15
+
+
+
+    //  Ans 3
+
+//     var userName="Enter your name ";
+//   prompt(userName);
+//   alert("congrats");
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //  Assignment 3
+
+//   var city = prompt ("Enter your city  ");
+//   if ( city==="Karachi"){
+//     alert("Wellcome to the city of lights");
+//   }
+  
+  
+// var gender = prompt("Please Enter Your gender");
+// if (gender === "Male") {
+//   alert("Good Morning sir");
+// }
+
+// if (gender === "Female") {
+//   alert("Good Morning maam");
+// }
+
+
+
+// Ans 3
+
+// var liter = 100;
+
+// if (liter <= 0.25) {
+//   console.log("please refill the fuel in car");
+// }
+
+
